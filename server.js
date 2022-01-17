df2c9934ebd913159daba5294eb20bb6ed8cd19c
kstaver <staverkendra@gmail.com>
2022-01-16 20:45:14 -0800

    fixed possibel typos

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost/budget";
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(MONGODB_URL, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
